6f52d7d6d155b8057e71d757316ef44107d51015
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-12-05 16:21:34 +0100

    Fix package conflict

--- freeipa/config.go
+++ freeipa/config.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/tehwalris/go-freeipa/freeipa"
+	ipa "github.com/tehwalris/go-freeipa/freeipa"
 )
 
 // Config is the configuration parameters for the FreeIPA API
@@ -17,14 +17,14 @@ type Config struct {
 }
 
 // Client creates a FreeIPA client scoped to the global API
-func (c *Config) Client() (*freeipa.Client, error) {
+func (c *Config) Client() (*ipa.Client, error) {
 	tspt := &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: c.InsecureSkipVerify,
 		},
 	}
 
-	client, err := freeipa.Connect(c.Host, tspt, c.Username, c.Password)
+	client, err := ipa.Connect(c.Host, tspt, c.Username, c.Password)
 	if err != nil {
 		return nil, err
 	}
